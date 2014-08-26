c7cbe32c580106ce510022eaeed47682d6b7108a
Aaron Enright <fact50@gmail.com>
2014-08-26 16:48:24 -0500

    added test for an error from noaa

--- weather.py
+++ weather.py
@@ -12,19 +12,20 @@ def getweathernoaa(loc):
     return pywapi.get_weather_from_noaa(loc)
 
 def formatweather(wdict):
-    location = wdict["location"]
-    pressure = wdict["pressure_string"]
-    temp = wdict["temperature_string"]
-    time = wdict["observation_time"]
-    weatherstring = """
-    %s
-    %s
-    air pressure %s
-    %s
-    """ % (location, temp, pressure, time)
-    return weatherstring
+    if wdict == {'error': 'Could not connect to NOAA'}:
+        print wdict["error"]
+    else:
+        location = wdict["location"]
+        pressure = wdict["pressure_string"]
+        temp = wdict["temperature_string"]
+        time = wdict["observation_time"]
+        weatherstring = """
+        %s
+        %s
+        air pressure %s
+        %s
+        """ % (location, temp, pressure, time)
+        return weatherstring
 
 if __name__ == '__main__':
     print(formatweather(getweathernoaa(getlocation())))
-
-# {'error': 'Could not connect to NOAA'}
